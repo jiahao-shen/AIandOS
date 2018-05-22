df99302030e0fe5431d09a279926f6a36fb2f852
shenjiahao <1843781563@qq.com>
2018-05-23 03:13:04 +0800

    Add FileSystem.

---
+++ src/os/FileSystem.kt
@@ -0,0 +1,274 @@
+package os
+
+import java.util.*
+
+
+val scan = Scanner(System.`in`)
+
+/**
+ * @param fileName(文件名称)
+ * @param type(文件类型,0文件,1文件夹)
+ * @param permission(权限,0不可读不可写,1可读不可写,2可读可写
+ * @param content(文件内容)
+ * @param fatherNode(父目录)
+ */
+class FileNode(var fileName: String, var type: Int, var permission: Int, var size: Int, var content: String?, var fatherNode: FileNode?)
+
+val fileList = ArrayList<FileNode>()
+//val currentFileList
+lateinit var currentPath: FileNode
+
+lateinit var username: String
+
+lateinit var password: String
+
+fun initFileSystem() {
+    val rootNode = FileNode("/", 1, 2, 0, null, null)
+    currentPath = rootNode
+    fileList.add(rootNode)
+    fileList.add(FileNode("bin", 1, 2, 0, null, rootNode))
+    fileList.add(FileNode("usr", 1, 2, 0, null, rootNode))
+    fileList.add(FileNode("etc", 1, 2, 0, null, rootNode))
+}
+
+//登录
+fun login() {
+    val USERNAME = "sam"
+    val PASSWORD = "258667"
+    while (true) {
+        print("Please enter your username:")
+        username = scan.next()
+        print("Please enter your password:")
+        password = scan.next()
+        if (username == USERNAME && password == PASSWORD) {
+            println("Login Success")
+            return
+        } else {
+            println("Password Error")
+        }
+    }
+}
+
+//列出当前路径的所有文件和文件夹
+fun listAllFile() {
+    System.out.printf("%-15s%-15s%-15s\n", "Name", "Size", "Permission")
+    println("-----------------------------------------------------------")
+    for (item in fileList) {
+        if (item.fatherNode == currentPath) {
+            System.out.printf("%-15s%-15s%-15s\n", item.fileName, item.size, item.permission)
+        }
+    }
+
+}
+
+//改变当前路径
+fun changeDir(name: String) {
+    when (name) {
+        ".." -> {       //退到父目录
+            if (currentPath.fatherNode != null) {
+                currentPath = currentPath.fatherNode!!
+            }
+        }
+        else -> {
+            for (item in fileList) {
+                if (item.fatherNode == currentPath && item.type == 1 && item.fileName == name) {        //找到对应文件名的文件
+                    if (item.permission == 0) {     //没有进入权限
+                        println("cd: permission denied: $name")
+                    } else {    //否则更改当前路径
+                        currentPath = item
+                    }
+                    return
+                }
+            }
+            println("cd: no such directory: $name")     //没有该目录
+        }
+    }
+}
+
+//创建文件夹
+fun createDir(name: String) {
+    for (item in fileList) {
+        if (item.fatherNode == currentPath && item.type == 1 && item.fileName == name) {    //文件夹已经存在
+            println("mkdir: $name: File exists")
+            return      //直接return
+        }
+    }
+    if (currentPath.permission == 2)        //当前文件夹有权限修改
+        fileList.add(FileNode(name, 1, 2, 0, null, currentPath))
+    else {
+        println("You don't have permission to create a directory")      //当前文件夹没有权限创建
+    }
+}
+
+//删除文件
+fun removeFile(name: String) {
+    for (item in fileList) {
+        if (item.fatherNode == currentPath && item.fileName == name) {      //找到对应文件
+            if (currentPath.permission == 2) {     //当前路径有删除权限
+                print("Are you sure to delete $name? ")
+                when (scan.next()) {
+                    "Yes", "yes" -> {
+                        fileList.remove(item)
+                        var node = currentPath      //递归更新大小
+                        while (true) {
+                            node.size -= item.size
+                            if (node.fatherNode == null)
+                                break
+                            else
+                                node = node.fatherNode!!
+                        }
+                    }
+                }
+            } else {        //当前路径没有删除权限
+                println("rm: $name: Permission denied")
+            }
+            return
+        }
+    }
+}
+
+//展示当前路径
+fun showCurrentPath() {
+    val path = ArrayList<String>()
+    var node = currentPath
+    while (true) {
+        if (node.fatherNode != null) {
+            path.add(node.fileName)
+            node = node.fatherNode!!
+        } else {
+            break
+        }
+    }
+    path.reverse()
+    if (path.size == 0)
+        println("/")
+    else {
+        for (item in path) {
+            print("/$item")
+        }
+        println()
+    }
+}
+
+fun vimFile(name: String) {
+    for (item in fileList) {
+        if (item.fatherNode == currentPath && item.fileName == name) {      //根据名字查找
+            when (item.type) {
+                0 -> {          //如果是文件夹则不能编辑
+                    println("You cannot edit a directory")
+                    return
+                }
+                1 -> {      //否则直接编辑
+                    writeFile(item)
+                }
+            }
+        }
+    }
+    createFile(name)        //创建新文件
+}
+
+fun createFile(name: String) {
+    if (currentPath.permission == 2) {      //当前路径有写权限
+        val file = FileNode(name, 0, 2, 0, null, currentPath)       //创建新文件
+        writeFile(file)     //写文件
+        fileList.add(file)      //添加
+        println("Create file success")
+    } else {
+        println("You don't have permission to edit file")       //当前路径没有写权限
+    }
+}
+
+//写文件
+fun writeFile(file: FileNode) {
+    when (file.permission) {        //文件权限判断
+        0, 1 -> {       //没有写权限
+            println("You don't have permission to write")
+            return
+        }
+        2 -> {      //有写权限
+            println("Input content(end with enter):")
+            val content = scan.nextLine()
+            file.content = content
+            file.size = content.length
+            var node = currentPath
+            while (true) {
+                node.size += file.size
+                if (node.fatherNode == null)
+                    break
+                else
+                    node = node.fatherNode!!
+            }
+        }
+    }
+}
+
+//读文件
+fun readFile(name: String) {
+    for (item in fileList) {
+        if (item.fatherNode == currentPath && item.fileName == name) {
+            if (item.type == 1) {       //不能读取一个文件夹
+                println("You cannot read a directory")
+                return
+            }
+            when (item.permission) {
+                0 -> {      //没有读权限
+                    println("You don't have permission to read")
+                    return
+                }
+                else -> {       //有读权限则输出内容
+                    println("The content of file:")
+                    println(item.content)
+                }
+            }
+        }
+    }
+}
+
+//更改权限
+fun changePermission(name: String, permission: Int) {
+    for (item in fileList) {
+        if (item.fatherNode == currentPath && item.fileName == name) {
+            item.permission = permission
+            return
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    initFileSystem()
+
+    login()
+
+    while (true) {
+        val input = scan.nextLine().split("\\s+".toRegex())
+        when (input[0]) {
+            "ls" -> {
+                listAllFile()
+            }
+            "mkdir" -> {
+                createDir(input[1])
+            }
+            "cd" -> {
+                changeDir(input[1])
+            }
+            "rm" -> {
+                removeFile(input[1])
+            }
+            "vim" -> {
+                vimFile(input[1])
+            }
+            "cat" -> {
+                readFile(input[1])
+            }
+            "exit" -> {
+                System.exit(0)
+            }
+            "pwd" -> {
+                showCurrentPath()
+            }
+            "chmod" -> {
+                changePermission(input[2], input[1].toInt())
+            }
+        }
+    }
+}
